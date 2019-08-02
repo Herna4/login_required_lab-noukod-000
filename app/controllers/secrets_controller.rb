2bd0628e305b0b6f91ad1563011ee10f56ea23ed
Herna Michel <labellehernamichel@yahoo.fr>
2019-08-02 11:42:44 -0400

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,4 +1,13 @@
 class SecretsController < ApplicationController
+  before_action :require_logged_in
+
   def new
   end
+  
+  private
+
+  def require_logged_in
+    redirect_to controller: 'sessions', action: 'new' unless current_user
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   get 'secrets/new'
 
-  get 'sessions/new'
+
+  root 'application#hello'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret' => 'secrets#show'
 
 end
+# root 'sessions#new'
+#   post 'create' => 'sessions#create'
+#   post 'destroy' => 'sessions#destroy'
+#   get 'welcome' => 'application#welcome'
+#   get 'show' => 'secrets#show'
